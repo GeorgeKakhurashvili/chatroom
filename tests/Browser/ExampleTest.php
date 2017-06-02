1a0c1a85e888877b22b5808f7c971ee70cfe106f
giorgi <giokaxura@yahoo.com>
2017-06-02 15:40:23 +0400

    chatroom

---
+++ tests/Browser/ExampleTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Browser;
+
+use App\User;
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class ExampleTest extends DuskTestCase
+{
+    use DatabaseMigrations;
+    /**
+     * A basic browser test example.
+     *
+     * @return void
+     */
+    public function testBasicExample()
+    {
+      $user1 = factory(User::class)->create([
+        "name"=>"Jone Doe"
+      ]);
+
+      $user2 = factory(User::class)->create([
+        "name"=>"Jane Doe"
+      ]);
+      $this->browse(function ($first, $second) use($user1,$user2) {
+        $first->loginAs($user1)
+              ->visit('/chat')
+              ->waitFor('.chat-composer');
+
+        $second->loginAs($user2)
+               ->visit('/chat')
+               ->waitFor('.chat-composer')
+               ->type('#message', 'Hey Jone')
+               ->press('Send');
+
+        $first->waitForText('Hey Jone')
+              ->assertSee('Jane Doe');
+      });
+    }
+}
